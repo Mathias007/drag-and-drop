8deff00caff7238783c508ae9fffafbf8a57bd9a
Mathias007 <mathias.najlepszy@gmail.com>
2021-03-03 20:20:39 +0100

    fix collision between dragging and editing tasks.

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ let listArrays = [];
 
 // Drag Functionality
 let draggedItem;
+let dragging = false;
 let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
@@ -167,15 +168,20 @@ function rebuildArrays() {
 function updateItem(id, column) {
     const selectedArray = listArrays[column];
     const selectedColumnEl = listColumns[column].children;
-    if (!selectedColumnEl[id].textContent) {
-        delete selectedArray[id];
+    if (!dragging) {
+        if (!selectedColumnEl[id].textContent) {
+            delete selectedArray[id];
+        } else {
+            selectedArray[id] = selectedColumnEl[id].textContent;
+        }
+        updateDOM();
     }
-    updateDOM();
 }
 
 // When Item Starts Dragging
 function drag(e) {
     draggedItem = e.target;
+    dragging = true;
 }
 
 // Column Allows for Item to Drop
@@ -199,6 +205,8 @@ function drop(e) {
     // Add Item to Column
     const parent = listColumns[currentColumn];
     parent.appendChild(draggedItem);
+    // Dragging complete
+    dragging = false;
     rebuildArrays();
 }
 
